796aaef3f6cfbd3c7c56c9a90c31e72954f91673
moniCoder <monicamartin12893@gmail.com>
2016-11-16 15:30:26 -0500

    Added files via upload. Practice java classes for coding solutions

---
+++ HashPractice.java
@@ -0,0 +1,14 @@
+/**
+ * Monica Martin
+ * Java Practice
+ * 
+ * Hash table practice
+ */
+
+public class HashPractice {
+    
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+    
+}
